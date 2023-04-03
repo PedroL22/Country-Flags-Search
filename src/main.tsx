a3ce0f23057ca8b82ba670132518c56196b70f53
Pedro Lucena <pedrocontact22@gmail.com>
2023-04-03 10:05:59 -0300

    📦 chore: Update packages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import ThemeContextProvider from './hooks/useTheme'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeContextProvider>
       <div className='bg-gray-200 dark:bg-gray-800 ease-in transition-all duration-75'>
